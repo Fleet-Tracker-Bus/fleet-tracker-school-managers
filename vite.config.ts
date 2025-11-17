01d7826c657e9c9c98b4ac1011e68a2d520a0303
MOHAMED ABOUAMMAR <75488196+med20201@users.noreply.github.com>
2025-11-17 04:37:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  base: "/",          // ensures assets and routing work at root
+  build: {
+    outDir: "dist",   // Vercel will serve files from here
+  },
+});
